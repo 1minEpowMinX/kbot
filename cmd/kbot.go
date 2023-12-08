e6db73e2187af504533a4716526bbad506728ca5
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2023-12-08 22:10:29 +0000

    v1.0.1 patch for adding message handling

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,19 +1,28 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	//TeleToken bot
+	TeleToken = os.Getenv("TELE_TOKEN")
 )
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
-	Use:   "kbot",
-	Short: "A brief description of your command",
+	Use:     "kbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,7 +30,25 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("kbot called")
+		fmt.Printf("kbot %s started", appVersion)
+
+		kbot, err := telebot.NewBot(telebot.Settings{
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. Error: s%", err)
+			return
+		}
+
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			payload := m.Message().Payload
+			log.Print(payload, m.Text())
+
+			return err
+		})
+		kbot.Start()
 	},
 }
 
--- go.mod
+++ go.mod
@@ -3,7 +3,14 @@ module github.com/1minepowminx/kbot
 go 1.21.4
 
 require (
+	github.com/spf13/cobra v1.8.0
+	github.com/tucnak/telebot v2.0.0+incompatible
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/mitchellh/hashstructure v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	gopkg.in/telebot.v3 v3.2.1
 )
