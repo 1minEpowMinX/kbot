cc8f37c995df7693d7982b186be1fec884ff7e56
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2024-01-06 06:33:46 +0000

    update pipeline

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -6,57 +6,54 @@ pipeline {
 		choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: "Pick ARCH")
 	}
 
-	// environment {
-	// 	REPO = 'https://github.com/1minEpowMinX/kbot'
-	// 	BRANCH = 'main'
-	// }
+	environment {
+		REPO = 'https://github.com/1minEpowMinX/kbot'
+		BRANCH = 'main'
+	}
 
 	stages {
 
-		stage('Example') {
+		stage("clone") {
 			steps {
-				echo 'Build for platform ${params.OS}'
-				echo 'Build for arch ${params.ARCH}'
+				echo 'Clone repository'
+				git (
+					branch: '${BRANCH}', 
+					url: '${REPO}',
+					credentialsId: 'github'
+				) //trouble spot
 			}
 		}
 
-	// 	stage("clone") {
-	// 		steps {
-	// 			echo 'Clone repository'
-	// 			git branch: '${BRANCH}', url: '${REPO}' //trouble spot
-	// 		}
-	// 	}
+		stage("test") {
+			steps {
+				echo 'Test exec started'
+				sh 'make test'
+			}
+		}
 
-	// 	stage("test") {
-	// 		steps {
-	// 			echo 'Test exec started'
-	// 			sh 'make test'
-	// 		}
-	// 	}
+		stage("build") {
+			steps {
+				echo 'Build exec started'
+				sh 'make build GOOS=${params.OS} GOARCH=${params.ARCH}'
+			}
+		}
 
-	// 	stage("build") {
-	// 		steps {
-	// 			echo 'Build exec started'
-	// 			sh 'make build GOOS=${params.OS} GOARCH=${params.ARCH}'
-	// 		}
-	// 	}
+		stage("image") {
+			steps {
+				echo 'Image build exec started'
+				sh 'make image'
+			}
+		}
 
-	// 	stage("image") {
-	// 		steps {
-	// 			echo 'Image build exec started'
-	// 			sh 'make image'
-	// 		}
-	// 	}
-
-	// 	stage("push") {
-	// 		steps {
-	// 			echo 'Image push exec started'
-	// 			script {
-	// 				docker.withRegistry('', 'dockerhub') {
-	// 					sh 'make push'
-	// 				}
-	// 			}
-	// 		}
-	// 	}
+		stage("push") {
+			steps {
+				echo 'Image push exec started'
+				script {
+					docker.withRegistry('', 'dockerhub') {
+						sh 'make push'
+					}
+				}
+			}
+		}
 	}
 }
