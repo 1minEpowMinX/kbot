463f35f81841ab2568d240f076c73e5e527a801a
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2024-01-06 06:48:26 +0000

    update steps desc

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -15,35 +15,35 @@ pipeline {
 
 		stage("clone") {
 			steps {
-				echo 'Clone repository'
+				echo 'CLONE REPOSITORY'
 				git branch: 'main', credentialsId: 'github', url: 'https://github.com/1minEpowMinX/kbot'
 			}
 		}
 
 		stage("test") {
 			steps {
-				echo 'Test exec started'
+				echo 'TEST EXEC STARTED'
 				sh 'make test'
 			}
 		}
 
 		stage("build") {
 			steps {
-				echo 'Build exec started'
+				echo 'BUILD EXEC STARTED'
 				sh 'make build GOOS=${params.OS} GOARCH=${params.ARCH}'
 			}
 		}
 
 		stage("image") {
 			steps {
-				echo 'Image build exec started'
+				echo 'IMAGE BUILD EXEC STARTED'
 				sh 'make image'
 			}
 		}
 
 		stage("push") {
 			steps {
-				echo 'Image push exec started'
+				echo 'IMAGE PUSH EXEC STARTED'
 				script {
 					docker.withRegistry('', 'dockerhub') {
 						sh 'make push'
