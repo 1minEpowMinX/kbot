e3a493d9bab98636b685184a2f0941b3ad9b75b5
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2024-01-06 06:18:40 +0000

    updated in accordance with the task

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -6,50 +6,57 @@ pipeline {
 		choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: "Choice ARCH")
 	}
 
-	environment {
-		REPO = 'https://github.com/1minEpowMinX/kbot'
-		BRANCH = 'main'
-	}
+	// environment {
+	// 	REPO = 'https://github.com/1minEpowMinX/kbot'
+	// 	BRANCH = 'main'
+	// }
 
 	stages {
 
-		stage("clone") {
+		stage('Example') {
 			steps {
-				echo 'Clone repository'
-				git branch: '${BRANCH}', url: '${REPO}'
+				echo 'Build for platform ${params.OS}'
+				echo 'Build for arch ${params.ARCH}'
 			}
 		}
 
-		stage("test") {
-			steps {
-				echo 'Test exec started'
-				sh 'make test'
-			}
-		}
+	// 	stage("clone") {
+	// 		steps {
+	// 			echo 'Clone repository'
+	// 			git branch: '${BRANCH}', url: '${REPO}' //trouble spot
+	// 		}
+	// 	}
 
-		stage("build") {
-			steps {
-				echo 'Build exec started'
-				sh 'make build GOOS=${params.OS} GOARCH=${params.ARCH}'
-			}
-		}
+	// 	stage("test") {
+	// 		steps {
+	// 			echo 'Test exec started'
+	// 			sh 'make test'
+	// 		}
+	// 	}
 
-		stage("image") {
-			steps {
-				echo 'Image build exec started'
-				sh 'make image'
-			}
-		}
+	// 	stage("build") {
+	// 		steps {
+	// 			echo 'Build exec started'
+	// 			sh 'make build GOOS=${params.OS} GOARCH=${params.ARCH}'
+	// 		}
+	// 	}
 
-		stage("push") {
-			steps {
-				echo 'Image push exec started'
-				script {
-					docker.withRegistry('', 'dockerhub') {
-						sh 'make push'
-					}
-				}
-			}
-		}
+	// 	stage("image") {
+	// 		steps {
+	// 			echo 'Image build exec started'
+	// 			sh 'make image'
+	// 		}
+	// 	}
+
+	// 	stage("push") {
+	// 		steps {
+	// 			echo 'Image push exec started'
+	// 			script {
+	// 				docker.withRegistry('', 'dockerhub') {
+	// 					sh 'make push'
+	// 				}
+	// 			}
+	// 		}
+	// 	}
 	}
 }
