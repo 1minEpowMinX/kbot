6d06e1b243c94a6e922924d178eba5f837fab821
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2024-01-21 08:28:05 +0000

    add monitoring instrumental

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -4,19 +4,68 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"os"
 	"time"
 
 	"github.com/spf13/cobra"
+
+	"github.com/hirosassa/zerodriver"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
 	telebot "gopkg.in/telebot.v3"
 )
 
 var (
-	//TeleToken bot
+	// TeleToken bot
 	TeleToken = os.Getenv("TELE_TOKEN")
+	// Metrics exporter host:port
+	MetricsHost = os.Getenv("METRICS_HOST")
 )
+
+// init OTLP
+func initMetrics(ctx context.Context) {
+	// Create a new OTLP Metrics gRPC exporter
+	exporter, _ := otlpmetricgrpc.New(
+		ctx,
+		otlpmetricgrpc.WithEndpoint(MetricsHost),
+		otlpmetricgrpc.WithInsecure(),
+	)
+
+	// Define the res with attributes to all metrics
+	// labels/tags/resources
+	resource := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String(fmt.Sprintf("kbot_%s", appVersion)),
+	)
+
+	// Create a new Metric Provider
+	mp := sdkmetric.NewMeterProvider(
+		sdkmetric.WithResource(resource),
+		sdkmetric.WithReader(
+			// collects and exports data every 10 sec
+			sdkmetric.NewPeriodicReader(exporter, sdkmetric.WithInterval(10*time.Second)),
+		),
+	)
+
+	otel.SetMeterProvider(mp)
+
+}
+
+func pmetrics(ctx context.Context, payload string) {
+	// Get the global MeterProvider and create a new Meter
+	meter := otel.GetMeterProvider().Meter("kbot_light_signal_counter")
+
+	// Get or create an Int64Counter instrument
+	counter, _ := meter.Int64Counter(fmt.Sprintf("kbot_light_signal_%s", payload))
+
+	// Add a value of 1 to the Int64Counter
+	counter.Add(ctx, 1)
+}
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
@@ -30,7 +79,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("kbot %s started", appVersion)
+		logger := zerodriver.NewProductionLogger()
 
 		kbot, err := telebot.NewBot(telebot.Settings{
 			Token:  TeleToken,
@@ -38,21 +87,47 @@ to quickly create a Cobra application.`,
 		})
 
 		if err != nil {
-			log.Fatalf("Please check TELE_TOKEN env variable. Error: s%", err)
+			logger.Fatal().Str("Error", err.Error()).Msg("Please check TELE_TOKEN")
 			return
+		} else {
+			logger.Info().Str("Version", appVersion).Msg("kbot started")
 		}
 
+		trafficSignal := make(map[string]map[string]int8)
+
+		trafficSignal["red"] = make(map[string]int8)
+		trafficSignal["amber"] = make(map[string]int8)
+		trafficSignal["green"] = make(map[string]int8)
+
+		trafficSignal["red"]["pin"] = 12
+		trafficSignal["amber"]["pin"] = 27
+		trafficSignal["green"]["pin"] = 22
+
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			logger.Info().Str("Payload", m.Text()).Msg(m.Message().Payload)
+
 			payload := m.Message().Payload
-			log.Print(payload, m.Text())
+			pmetrics(context.Background(), payload)
 
 			switch payload {
 			case "hello":
 				err = m.Send(fmt.Sprintf("Hello. I`m kbot %s!", appVersion))
-			}
 
+			case "red", "amber", "green":
+				if trafficSignal[payload]["on"] == 0 {
+					trafficSignal[payload]["on"] = 1
+				} else {
+					trafficSignal[payload]["on"] = 0
+				}
+
+				err = m.Send(fmt.Sprintf("Switch %s light signal to %d", payload, trafficSignal[payload]["on"]))
+
+			default:
+				m.Send("Usage /s red|amber|green")
+			}
 			return err
 		})
+
 		kbot.Start()
 	},
 }
@@ -69,4 +144,10 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// kbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func init() {
+	ctx := context.Background()
+	initMetrics(ctx)
+	rootCmd.AddCommand(kbotCmd)
 }
